2b11f063aac4363f991aa48f675be9af8f8fa435
Taurai Muzembe <tmuzembe@outlook.com>
2026-07-24 15:45:01 +0200

    update splash screen

--- app/src/main/java/com/example/msh_verify/SplashActivity.java
+++ app/src/main/java/com/example/msh_verify/SplashActivity.java
@@ -1,29 +1,32 @@
 package com.example.msh_verify;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        logo = findViewById(R.id.imgLogo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(animation);
+
         new Handler().postDelayed(() -> {
-
-            Intent intent = new Intent(SplashActivity.this,
-                    LoginActivity.class);
-
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
-
-        },3000);
-
+        }, 3000);
     }
 }
